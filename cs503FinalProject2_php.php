c77a604585511f0aac268d6607b888317f34eb1c
danielmatteson216 <danielmatteson216@gmail.com>
2017-11-28 21:28:49 -0800

    Add files via upload

---
+++ cs503FinalProject2_php.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+      <meta charset="UTF-8">
+      <title>cs503_php page</title>
+
+  </head>
+
+
+  <body>
+
+    <?php
+#php begins
+    #date
+    date_default_timezone_set("America/Los_Angeles");
+    echo date('h:i:s a') . "</br>";
+    echo "create a username n shit!";
+
+# php end
+    ?>
+
+
+  </body>
+
+
+</html>
