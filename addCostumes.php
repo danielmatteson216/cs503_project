a432e251c462ad44d62985aafa78446173acff47
jagelid <michelle@jagelid.com>
2017-12-03 17:49:50 -0800

    fixed styling for addCostumes and login

--- addCostumes.php
+++ addCostumes.php
@@ -2,23 +2,19 @@
 <html>
 <head>
 	<title>Modify costumes</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="form.css">
 </head>
 <body>
+	<div>
+		<a href="frontpage.php"> <-- Go back to main page</a>
+	</div>	
 
-
-<div>
 	<form action="processCostume.php" method="POST">
-		<label>Name of costume: <input type="text" name="name" placeholder="name" required></label><br>
-		<label>Image url: <input type="text" name="url" placeholder="url" required> </label><br>
-		<label>Costume condition:<input type="text" name="condition" pattern="^[1-10]" title="Enter a value on a scale 1-10" required> </label><br>
-		<label>Description: <input type="text" name="description" required> </label><br>
+		<label>Name of costume: <br><input type="text" name="name" placeholder="name" required></label><br>
+		<label>Image url: <br><input type="text" name="url" placeholder="url" required> </label><br>
+		<label>Costume condition:<br><input type="text" name="condition" pattern="^[1-10]" title="Enter a value on a scale 1-10" required> </label><br>
+		<label>Description: <br><input type="text" name="description" required> </label><br>
 		<input type="submit" value="Add costume">
 	</form>
-</div>
-<div>
-		<a href="frontpage.php"> <-- Go back to main page</a>
-</div>	
-
 </body>
 </html>
